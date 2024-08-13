63d1d5c34e8be7da900cd77f6cbd9266a0e0913f
msajid103 <sajid2021@namal.edu.pk>
2024-08-12 22:09:41 -0700

    First commit

---
+++ notes/noteapp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
+from django.utils import timezone
+
+# Create your models here.
+class Note(models.Model):
+    CATEGORY = (('BUSINESS','Business'),
+                ('PERSONAL','Personal'),
+                ('IMPORTANT', 'Important'))
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    category = models.CharField(max_length=15, choices=CATEGORY, default="PERSONAL")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args, **kwargs):
+       
+        slug_base = slugify(self.title)
+        slug = slug_base
+        if Note.objects.filter(slug = slug).exists():
+            slug = f'{slug_base}-{get_random_string(5)}'
+        self.slug = slug
+        self.updated = timezone.now()
+
+        super(Note, self).save(*args, **kwargs)
---
+++ notes/noteapp/views.py
@@ -0,0 +1,41 @@
+from .models import Note
+from noteapp.serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+# Create your views here.
+@api_view(['GET','POST'])
+def notes(request):
+    if request.method == "GET":
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many = True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = NoteSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET','PUT', 'DELETE'])
+def note_detail(request, slug):
+    try:
+        note = Note.objects.get(slug =slug)
+    except Note.DoesNotExist:
+        return Response(status= status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serialzer = NoteSerializer(note)
+        return Response(serialzer.data)
+    
+    elif request.method == 'PUT':
+        serialzer = NoteSerializer(note , request.data)
+        if serialzer.is_valid():
+            serialzer.save()
+            return Response(serialzer.data, status=status.HTTP_200_OK)
+        
+        return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
